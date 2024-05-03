3388ca4d9c5e5139aee27527863a811e376148ec
jimin5953 <162667538+jimin5953@users.noreply.github.com>
2024-05-03 15:11:01 +0900

    Add files via upload

---
+++ 0503/src/DValue.js
@@ -0,0 +1,16 @@
+import { useDebugValue, useState } from "react";
+function useCounter(initialValue) {
+  const [count, setCount] = useState(initialValue);
+  useDebugValue(count, (count) => `Count: ${count}`);
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+  return {
+    count,
+    handleIncrement,
+    handleDecrement,
+  };
+}
